042a71a57b7e1354827df6fb504a478ac7957ed0
Jascen <Devin.Ulrey@gmail.com>
2015-08-27 22:03:30 -0400

    Added Res Kill Protection.  Currently lasts for 15s, but is configurable.

---
+++ Scripts/SpecialSystems/ResKill Protection/RKPOff.cs
@@ -0,0 +1,35 @@
+﻿using Server;
+using Server.Mobiles;
+using Server.Network;
+using Server.Misc;
+using System;
+
+namespace Server.Commands
+{
+    class RKPOff
+    {
+        private static Mobile m_caller;
+
+        public static void Initialize()
+        {
+            CommandSystem.Register("RK", AccessLevel.Player, new CommandEventHandler(OnCommand));
+        }
+        public static void OnCommand ( CommandEventArgs e )
+        {
+            m_caller = e.Mobile;
+			
+			if ( m_caller.Blessed == false )
+			{
+				m_caller.SendMessage( "You're already mortal!");
+			}
+			else
+			{
+				m_caller.Blessed = false;
+				m_caller.FixedParticles( 0x376A, 10, 15, 5018, EffectLayer.Waist );
+				m_caller.PlaySound( 513 );
+				m_caller.SendMessage( "You're now mortal as you wished.");
+
+			}
+        }
+    }
+}
